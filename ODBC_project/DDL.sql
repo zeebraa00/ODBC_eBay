c268a2ac0d44791fbd8148396e07fa4b06613142
zeebraa00 <zeebraa00@gmail.com>
2020-11-16 20:38:58 +0900

    [edit] DDL 제발 돼라 제 발 제 발

--- ODBC_project/DDL.sql
+++ ODBC_project/DDL.sql
@@ -1,50 +1,64 @@
 create table user (
-	uid		int AUTO_INCREMENT,
-	name		varchar(20) not null,
-	email      varchar(20) not null,
-	password   varchar(20),
+	uid int AUTO_INCREMENT,
+	name varchar(20) not null,
+	email varchar(20) not null,
+	pw varchar(20),
     primary key (uid)
 );
+
 create table item (
-	id      		int AUTO_INCREMENT,
-    uid    		int,
-	category       varchar(20),
-	description    varchar(100),
-    `condition`      varchar(10) check (`condition` in ('new', 'like-new', 'very-good', 'good', 'acceptable')),
-    latest_bid     numeric(5,0),
-    buy_it_now     numeric(5,0),
-    status         varchar(20),
-    posted_date    datetime,
-    end_date       datetime,
+	id int AUTO_INCREMENT,
+    uid int,
+	category varchar(20),
+	description varchar(100),
+    cond varchar(10)
+		check (cond in ('new', 'like-new', 'very-good', 'good', 'acceptable')),
+    latest_bid numeric(5,0) NOT NULL,
+    buy_it_now numeric(5,0),
+    status varchar(20),
+    posted_date datetime,
+    end_date datetime,
 	primary key (id),
 	foreign key (uid) references user(uid)
+		on delete set null
 );
+
 create table transaction (
-	tid				int AUTO_INCREMENT,
-	id					int,
-	transaction_date	datetime,
-	seller_id			int,
-	buyer_id			int,
-	sell_price			numeric(5,0),
+	tid	int AUTO_INCREMENT,
+	id int,
+	transaction_date datetime,
+	seller_id int,
+	buyer_id int,
+	sell_price numeric(5,0),
 	primary key (tid),
-	foreign key (id) references item(id),
-	foreign key (seller_id) references user(uid),
-	foreign key (buyer_id) references user(uid),
+	foreign key (id) references item(id)
+		on delete set null,
+	foreign key (seller_id) references user(uid)
+		on delete set null,
+	foreign key (buyer_id) references user(uid)
+		on delete set null,
 	foreign key (sell_price) references item(latest_bid)
+		on delete set null
 );
+
 create table bid_history (
 	uid	int,
-	id		int,
+	id int,
 	bid_price numeric(5,0),
 	primary key (id, bid_price),
-	foreign key (uid) references user(uid),
-	foreign key (id) references item(id),
+	foreign key (uid) references user(uid)
+		on delete set null,
+	foreign key (id) references item(id)
+		on delete set null
 );
+
 create table watched (
-	uid		int,
-	id			int,
-	watchedAt	datetime,
+	uid	int,
+	id int,
+	watchedAt datetime,
 	primary key (uid, id, watchedAt),
-	foreign key (uid) references user(uid),
-	foreign key (id) references item(id),
+ 	foreign key (uid) references user(uid)
+	 	on delete set null,
+	foreign key (id) references item(id)
+		on delete set null
 );
