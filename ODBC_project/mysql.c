6d405c574d0f4ef568323a03dc4dc014f575d90b
zeebraa00 <zeebraa00@gmail.com>
2020-11-17 02:35:08 +0900

    [edit] modularization mysql connection

--- ODBC_project/mysql.c
+++ ODBC_project/mysql.c
@@ -8,6 +8,23 @@ char * ddl3 = "create table transaction (tid int AUTO_INCREMENT, id int, transac
 char * ddl4 = "create table bid_history (uid int, id int, bid_price numeric(5,0), primary key (id, bid_price), foreign key (uid) references user(uid) on delete cascade, foreign key (id) references item(id) on delete cascade );";
 char * ddl5 = "create table watched ( uid int, id int, watchedAt datetime, primary key (uid, id, watchedAt), foreign key (uid) references user(uid) on delete cascade, foreign key (id) references item(id) on delete cascade );";
 
+// Initialize mysql
+void sql_init(MYSQL *conn) {
+    if (conn==NULL) {
+        printf("mysql_init: %s\n", mysql_error(conn));
+        exit(1);
+    }
+}
+
+// Connect to database
+void sql_connect(MYSQL *conn) {
+    if (mysql_real_connect(conn, "localhost", "db16314728", "970321", "db16314728", 0, NULL,  0)==NULL){
+        printf("mysql_real_connect: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+}
+
 // Implement query
 void query(MYSQL * conn, char * sqlquery) {
     if (mysql_query(conn, sqlquery)){
@@ -41,21 +58,11 @@ void query_print(MYSQL * conn, char * sqlquery) {
 }
 
 int main(int argc, char* argv[]) {
-    
     // Initialize mysql
     MYSQL *conn = mysql_init(NULL);
-    if (conn==NULL) {
-        printf("mysql_init: %s\n", mysql_error(conn));
-        exit(1);
-    }
-    
+    sql_init(conn);
     // Connect to database
-    if (mysql_real_connect(conn, "localhost", "db16314728", "970321", "db16314728", 0, NULL,  0)==NULL){
-        printf("mysql_real_connect: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        exit(1);
-    }
-
+    sql_connect(conn);
     // Implement DDL
     query(conn, ddl1);query(conn, ddl2);query(conn, ddl3);query(conn, ddl4);query(conn, ddl5);
     
