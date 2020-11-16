464f00692812835d34110f1194ad130aba9c751c
zeebraa00 <zeebraa00@gmail.com>
2020-11-17 01:52:00 +0900

    [edit] DDL implementation on c code (success)

--- ODBC_project/mysql.c
+++ ODBC_project/mysql.c
@@ -1,69 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <mysql.h>
 
-char* ddl = "create table user ( \
-	uid int AUTO_INCREMENT, \
-	name varchar(20) not null, \
-	email varchar(20) not null, \
-	pw varchar(20), \
-    primary key (uid) \
-); \
- \
-create table item ( \
-	id int AUTO_INCREMENT, \
-    uid int, \
-	category varchar(20), \
-	description varchar(100), \
-    cond varchar(10) \
-		check (cond in ('new', 'like-new', 'very-good', 'good', 'acceptable')), \
-    latest_bid numeric(5,0) not null, \
-    buy_it_now numeric(5,0), \
-    status varchar(20), \
-    posted_date datetime, \
-    end_date datetime, \
-	primary key (id), \
-	foreign key (uid) references user(uid) \
-		on delete cascade \
-); \
- \
-create table transaction ( \
-	tid	int AUTO_INCREMENT, \
-	id int, \
-	transaction_date datetime, \
-	seller_id int, \
-	buyer_id int, \
-	sell_price numeric(5,0) not null, \
-	primary key (tid), \
-	foreign key (id) references item(id) \
-		on delete cascade, \
-	foreign key (seller_id) references user(uid) \
-		on delete cascade, \
-	foreign key (buyer_id) references user(uid) \
-		on delete cascade \
-); \
- \
-create table bid_history ( \
-	uid	int, \
-	id int, \
-	bid_price numeric(5,0), \
-	primary key (id, bid_price), \
-	foreign key (uid) references user(uid) \
-		on delete cascade, \
-	foreign key (id) references item(id) \
-		on delete cascade \
-); \
- \
-create table watched ( \
-	uid	int, \
-	id int, \
-	watchedAt datetime, \
-	primary key (uid, id, watchedAt), \
- 	foreign key (uid) references user(uid) \
-	 	on delete cascade, \
-	foreign key (id) references item(id) \
-		on delete cascade \
-);"
-
+char * ddl1 = "create table user (uid int AUTO_INCREMENT, name varchar(20) not null, email varchar(20) not null, pw varchar(20), primary key (uid) );";
+char * ddl2 = "create table item (id int AUTO_INCREMENT, uid int, category varchar(20), description varchar(100), cond varchar(10) check (cond in ('new', 'like-new', 'very-good', 'good', 'acceptable')), latest_bid numeric(5,0) not null, buy_it_now numeric(5,0), status varchar(20), posted_date datetime, end_date datetime, primary key (id), foreign key (uid) references user(uid) on delete cascade );";
+char * ddl3 = "create table transaction (tid int AUTO_INCREMENT, id int, transaction_date datetime, seller_id int, buyer_id int, sell_price numeric(5,0) not null, primary key (tid), foreign key (id) references item(id) on delete cascade, foreign key (seller_id) references user(uid) on delete cascade, foreign key (buyer_id) references user(uid) on delete cascade );";
+char * ddl4 = "create table bid_history (uid int, id int, bid_price numeric(5,0), primary key (id, bid_price), foreign key (uid) references user(uid) on delete cascade, foreign key (id) references item(id) on delete cascade );";
+char * ddl5 = "create table watched ( uid int, id int, watchedAt datetime, primary key (uid, id, watchedAt), foreign key (uid) references user(uid) on delete cascade, foreign key (id) references item(id) on delete cascade );";
+ 
 int main(int argc, char* argv[]) {
     
     // Initialize mysql
@@ -81,7 +25,27 @@ int main(int argc, char* argv[]) {
     }
 
     // Implement DDL
-    if (mysql_query(conn, ddl)){
+    if (mysql_query(conn, ddl1)){
+        printf("mysql_query: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+    if (mysql_query(conn, ddl2)){
+        printf("mysql_query: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+    if (mysql_query(conn, ddl3)){
+        printf("mysql_query: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+    if (mysql_query(conn, ddl4)){
+        printf("mysql_query: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+    if (mysql_query(conn, ddl5)){
         printf("mysql_query: %s\n", mysql_error(conn));
         mysql_close(conn);
         exit(1);
