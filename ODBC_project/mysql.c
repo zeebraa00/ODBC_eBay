cdc61b7e4eb70069f1f6822ca130eed32b5ecfd4
zeebraa00 <zeebraa00@gmail.com>
2020-11-17 00:17:55 +0900

    [add] implement DDL in c code

--- ODBC_project/mysql.c
+++ ODBC_project/mysql.c
@@ -1,46 +1,120 @@
 #include <stdio.h>
 #include <mysql.h>
-//#include <my_global.h>  // this is not needed in 8.0
+
+char* ddl = "create table user ( \
+	uid int AUTO_INCREMENT, \
+	name varchar(20) not null, \
+	email varchar(20) not null, \
+	pw varchar(20), \
+    primary key (uid) \
+); \
+ \
+create table item ( \
+	id int AUTO_INCREMENT, \
+    uid int, \
+	category varchar(20), \
+	description varchar(100), \
+    cond varchar(10) \
+		check (cond in ('new', 'like-new', 'very-good', 'good', 'acceptable')), \
+    latest_bid numeric(5,0) not null, \
+    buy_it_now numeric(5,0), \
+    status varchar(20), \
+    posted_date datetime, \
+    end_date datetime, \
+	primary key (id), \
+	foreign key (uid) references user(uid) \
+		on delete cascade \
+); \
+ \
+create table transaction ( \
+	tid	int AUTO_INCREMENT, \
+	id int, \
+	transaction_date datetime, \
+	seller_id int, \
+	buyer_id int, \
+	sell_price numeric(5,0) not null, \
+	primary key (tid), \
+	foreign key (id) references item(id) \
+		on delete cascade, \
+	foreign key (seller_id) references user(uid) \
+		on delete cascade, \
+	foreign key (buyer_id) references user(uid) \
+		on delete cascade \
+); \
+ \
+create table bid_history ( \
+	uid	int, \
+	id int, \
+	bid_price numeric(5,0), \
+	primary key (id, bid_price), \
+	foreign key (uid) references user(uid) \
+		on delete cascade, \
+	foreign key (id) references item(id) \
+		on delete cascade \
+); \
+ \
+create table watched ( \
+	uid	int, \
+	id int, \
+	watchedAt datetime, \
+	primary key (uid, id, watchedAt), \
+ 	foreign key (uid) references user(uid) \
+	 	on delete cascade, \
+	foreign key (id) references item(id) \
+		on delete cascade \
+);"
 
 int main(int argc, char* argv[]) {
-  MYSQL *conn = mysql_init(NULL);
-  if (conn==NULL){
-      printf("mysql_init: %s\n", mysql_error(conn));
-      exit(1);
-  }
+    
+    // Initialize mysql
+    MYSQL *conn = mysql_init(NULL);
+    if (conn==NULL) {
+        printf("mysql_init: %s\n", mysql_error(conn));
+        exit(1);
+    }
+    
+    // Connect to database
+    if (mysql_real_connect(conn, "localhost", "db16314728", "970321", "db16314728", 0, NULL,  0)==NULL){
+        printf("mysql_real_connect: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
 
-  if(mysql_real_connect(conn, "localhost", "db16314728", "970321", "db16314728", 0, NULL,  0)==NULL){
-      printf("mysql_real_connect: %s\n", mysql_error(conn));
-      mysql_close(conn);
-      exit(1);
-  }
+    // Implement DDL
+    if (mysql_query(conn, ddl)){
+        printf("mysql_query: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
 
-  char * sqlquery = "show tables;";
-  if(mysql_query(conn, sqlquery)){
-      printf("mysql_query: %s\n", mysql_error(conn));
-      mysql_close(conn);
-      exit(1);
-  }
+    // Implement query
+    char * sqlquery = "show tables;";
+    if (mysql_query(conn, sqlquery)){
+        printf("mysql_query: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
 
-  MYSQL_RES *result = mysql_store_result(conn);
-  if(result == NULL){
-      printf("mysql_store_result: %s\n", mysql_error(conn));
-      mysql_close(conn);
-      exit(1);
-  }
+    // Print the result of query
+    MYSQL_RES *result = mysql_store_result(conn);
+    if (result == NULL) {
+        printf("mysql_store_result: %s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
 
-  int num_fields = mysql_num_fields(result);
+    int num_fields = mysql_num_fields(result);
 
-  MYSQL_ROW row;
-  while ((row = mysql_fetch_row(result))) {
-      for(int i=0; i<num_fields; i++){
-          printf (" %s ", row[i]? row[i] : "NULL");
-      }
-      printf("\n");
-  }
+    MYSQL_ROW row;
+    while (( row = mysql_fetch_row(result) )) {
+        for (int i=0; i<num_fields; i++) {
+            printf (" %s ", row[i]? row[i] : "NULL");
+        }
+        printf("\n");
+    }
 
-  mysql_free_result(result);
-  mysql_close(conn);
+    mysql_free_result(result);
+    mysql_close(conn);
 
-  return 1;
+    return 0;
 }
