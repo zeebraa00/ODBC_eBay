c5cd918783ec0492f1d17e1efde12c30217802bf
zeebraa00 <zeebraa00@gmail.com>
2020-11-16 23:27:22 +0900

    [add] mysql connecting

--- ODBC_project/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sql.h>
-#include <sqlext.h>
-
-int main() {
-    
-    return 0;
-}
---
+++ ODBC_project/mysql.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <mysql.h>
+//#include <my_global.h>  // this is not needed in 8.0
+
+int main(int argc, char* argv[])
+{
+  MYSQL *conn = mysql_init(NULL);
+  if (conn==NULL){
+      printf("mysql_init: %s\n", mysql_error(conn));
+      exit(1);
+  }
+
+  if(mysql_real_connect(conn, "localhost", "db16314728", "970321", "db16314728", 0, NULL,  0)==NULL){
+      printf("mysql_real_connect: %s\n", mysql_error(conn));
+      mysql_close(conn);
+      exit(1);
+  }
+
+  char * sqlquery = "with temp(building, num) as \
+        (select building, count(course_id) \
+         from section group by building) \
+	select building \
+	from temp \
+	where temp.num = \
+        (select max(temp.num) from temp)";
+  if(mysql_query(conn, sqlquery)){
+      printf("mysql_query: %s\n", mysql_error(conn));
+      mysql_close(conn);
+      exit(1);
+  }
+
+  MYSQL_RES *result = mysql_store_result(conn);
+  if(result == NULL){
+      printf("mysql_store_result: %s\n", mysql_error(conn));
+      mysql_close(conn);
+      exit(1);
+  }
+
+  int num_fields = mysql_num_fields(result);
+
+  MYSQL_ROW row;
+  while ((row = mysql_fetch_row(result))) {
+      for(int i=0; i<num_fields; i++){
+          printf (" %s ", row[i]? row[i] : "NULL");
+      }
+      printf("\n");
+  }
+
+  mysql_free_result(result);
+  mysql_close(conn);
+
+  return 1;
+}
